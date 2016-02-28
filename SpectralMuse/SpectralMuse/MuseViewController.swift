1b0ff9952b5124e61c78c087caeb0f64b11e4af3
David Conner <dconner.pro@gmail.com>
2016-02-27 22:27:00 -0500

    added a buddon

---
+++ SpectralMuse/SpectralMuse/MuseViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  SpectralMuse
+//
+//  Created by David Conner on 2/27/16.
+//  Copyright © 2016 Spectra. All rights reserved.
+//
+
+import Cocoa
+import Spectra
+import Swinject
+
+class MuseViewController: NSViewController {
+    
+    var device: MTLDevice!
+    var library: MTLLibrary!
+    var descMan: DescriptorManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        device = MTLCreateSystemDefaultDevice()
+        library = device!.newDefaultLibrary()
+        let bundle = NSBundle(forClass: ViewController.self)
+        let xmlData: NSData = S3DXML.readXML(bundle, filename: "DefaultPipeline")
+        let s3d = S3DXML(data: xmlData)
+        
+        descMan = DescriptorManager(library: library!)
+        descMan.parseS3DXML(s3d)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override var representedObject: AnyObject? {
+        didSet {
+            // Update the view, if already loaded.
+        }
+    }
+    
+    
+}
+
